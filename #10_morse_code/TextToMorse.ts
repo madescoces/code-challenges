9ae3468e8960f32b1c8c64ec14adcd5b08288ed1
madescoces <madescoces@gmail.com>
2024-01-23 11:24:15 -0300

    Morse code challenge #10 added

---
+++ #10_morse_code/TextToMorse.ts
@@ -0,0 +1,82 @@
+/*
+ * Reto #9
+ * CÓDIGO MORSE
+ * Fecha publicación enunciado: 02/03/22
+ * Fecha publicación resolución: 07/03/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que sea capaz de transformar texto natural a código morse y viceversa.
+ * - Debe detectar automáticamente de qué tipo se trata y realizar la conversión.
+ * - En morse se soporta raya "—", punto ".", un espacio " " entre letras o símbolos y dos espacios entre palabras "  ".
+ * - El alfabeto morse soportado será el mostrado en https://es.wikipedia.org/wiki/Código_morse.
+ */
+
+const ToMorseCode = (text: string): string => {
+  let result = ''
+  for (const word of text) {
+    if (word.toUpperCase() in morseCodeMap) {
+      result += morseCodeMap[word.toUpperCase()]
+    }
+  }
+  return result
+}
+
+const morseCodeMap = {
+  A: '.-',
+  B: '-...',
+  C: '-.-.',
+  D: '-..',
+  E: '.',
+  F: '..-.',
+  G: '--.',
+  H: '....',
+  I: '..',
+  J: '.---',
+  K: '-.-',
+  L: '.-..',
+  M: '--',
+  N: '-.',
+  O: '---',
+  P: '.--.',
+  Q: '--.-',
+  R: '.-.',
+  S: '...',
+  T: '-',
+  U: '..-',
+  V: '...-',
+  W: '.--',
+  X: '-..-',
+  Y: '-.--',
+  Z: '--..',
+  '0': '-----',
+  '1': '.----',
+  '2': '..---',
+  '3': '...--',
+  '4': '....-',
+  '5': '.....',
+  '6': '-....',
+  '7': '--...',
+  '8': '---..',
+  '9': '----.',
+  '.': '.-.-.-',
+  ',': '--..--',
+  '?': '..--..',
+  "'": '.----.',
+  '!': '-.-.--',
+  '/': '-..-.',
+  '(': '-.--.',
+  ')': '-.--.-',
+  '&': '.-...',
+  ':': '---...',
+  ';': '-.-.-.',
+  '=': '-...-',
+  '+': '.-.-.',
+  '-': '-....-',
+  _: '..--.-',
+  '"': '.-..-.',
+  $: '...-..-',
+  '@': '.--.-.',
+  ' ': '  ',
+}
+
+console.log('Result in morse code: ', ToMorseCode('Test text of morse code function: "hello world"!!!, sos'))
